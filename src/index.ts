42247ec1945886aec0b094370f59f5cc3aeafdab
tahsif <tahsif.cse@gmail.com>
2024-01-16 16:12:31 +0600

    Run process configure with typeScript+Express

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
