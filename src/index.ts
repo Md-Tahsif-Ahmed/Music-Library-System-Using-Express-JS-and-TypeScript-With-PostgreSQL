58d2d7d62150117be46b100ef3fe2372370ddab7
tahsif <tahsif.cse@gmail.com>
2024-01-17 11:57:31 +0600

    remove .env file

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,70 @@
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-import Knexfile from "../knexfile";
+// Import required modules
+import express, { Request, Response, NextFunction } from 'express';
+import * as bodyParser from 'body-parser';
+import Knex, { knex } from 'knex';
+import * as jwt from 'jsonwebtoken';
+import * as bcrypt from 'bcrypt';
+import { check, validationResult } from 'express-validator';
+require('dotenv').config();
 
-dotenv.config();
 
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASS);
+// Configure Knex for PostgreSQL
+const db = Knex({
+  client: 'pg',
+  connection: {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: "music_library",
+  },
+  debug: true,  
+  useNullAsDefault: true, 
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+const app = express();
+app.use(bodyParser.json());
+
+
+
+interface AuthenticatedRequest extends Request {
+  user?: any; // Here adjust the type of 'user' as needed
+}
+
+app.post('/register', [
+  check('username').isLength({ min: 5 }),
+  check('password').isLength({ min: 8 }),
+], async (req: Request, res: Response) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  try {
+    const { username, password } = req.body;
+
+    // Ensure 'password' is defined
+    if (!password) {
+      return res.status(400).json({ error: 'Password is required' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    // Insert user data into the "users" table
+    await db('users').insert({ password_hash: passwordHash, username: username });
+
+    res.sendStatus(201);
+  } catch (error: any) {
+    console.error('Error during registration:', error);
+    console.error(error.stack);   
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
